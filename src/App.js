11267a80c160057c99795effdc128ff11c6b99ac
amithreddy-pasham <amithreddypasham9@gmail.com>
2025-02-18 10:21:20 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Pages/Home/Navbar";
-import Home from "./Pages/Home/Homescreen/index"
+import Home from "./Pages/Home/Homescreen";
+
 function App() {
   return (
     <div className="App">
